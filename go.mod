e8a906c212bc027b8a26a34ec652ca625e004071
anastasia mac <fayst2996317@mail.ru>
2025-12-16 19:54:01 +0300

    my first application

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module my-package-kek
+module github.com/anaubreyt/my-package-kek
 
 go 1.25.4
---
+++ pkg/application/app.go
@@ -0,0 +1,7 @@
+package application
+
+import "fmt"
+
+func Run() {
+	fmt.Println("Сервер запущен на порту юхуху:8080")
+}
